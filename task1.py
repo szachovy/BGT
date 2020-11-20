ddff4b49a2a9336e1b2e30c021540aafc0dfb106
Wiktor <36889107+szachovy@users.noreply.github.com>
2020-11-20 16:58:00 +0100

    Task1
    Counting number of words in TEOFIL LENARTOWICZ literature https://literat.ug.edu.pl/teofil/index.htm#spis

---
+++ task1.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
+def accumulate(acc = 0):
+    for poem in range(1, 41):
+        acc += len(BeautifulSoup(requests.get("https://literat.ug.edu.pl/teofil/00{}.htm".format(poem)).text, "lxml").text.split()) - 6 if poem < 10 else len(BeautifulSoup(requests.get("https://literat.ug.edu.pl/teofil/0{}.htm".format(poem)).text, "lxml").text.split()) - 6
+    return acc
+    
+print(accumulate())
