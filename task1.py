2cf01697a8c179947d6805cba8c1696acd4c1ecb
Wiktor <36889107+szachovy@users.noreply.github.com>
2020-11-20 17:07:34 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,9 +1,11 @@
+# Zliczanie liczby słów wszystkich dzieł Teofila Lenartowicza
+
 from bs4 import BeautifulSoup
 import requests
 
 def accumulate(acc = 0):
     for poem in range(1, 41):
         acc += len(BeautifulSoup(requests.get("https://literat.ug.edu.pl/teofil/00{}.htm".format(poem)).text, "lxml").text.split()) - 6 if poem < 10 else len(BeautifulSoup(requests.get("https://literat.ug.edu.pl/teofil/0{}.htm".format(poem)).text, "lxml").text.split()) - 6
-    return acc
+    return acc 
     
-print(accumulate())
+print(accumulate()) # autor napisał 15341 słów
