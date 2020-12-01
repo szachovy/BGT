3b1cc6dc8e8a59a1d87439397dab1a99e7cd32f4
Wiktor <36889107+szachovy@users.noreply.github.com>
2020-12-01 10:41:26 +0100

    task2 nouns updated

--- task2.py
+++ task2.py
@@ -12,18 +12,55 @@
 #Zadanie należy rozwiązać w parach z zastosowaniem dowolnej technologii.
 #Należy wybrać autora, którego dzieła nie są chronione prawami autorskimi.
 
-from bs4 import BeautifulSoup
-import requests
-from collections import Counter
 
-def most_common(acc = []):
-    for poem in range(1, 71):
-        req = requests.get("https://literat.ug.edu.pl/roxolan/00{}.htm".format(poem)) if poem < 10 else requests.get("https://literat.ug.edu.pl/roxolan/0{}.htm".format(poem))
-        text = BeautifulSoup(req.content, "lxml").text.split()
-        acc += text[2:len(text)-2]
+        
+from bs4 import BeautifulSoup
+from collections import Counter
+from googletrans import Translator
+import spacy
+
+# -----------------
+
+class Common_Nouns(object):
+    def __init__(self):
+        self.translator = Translator(service_urls=['translate.googleapis.com'])                 
+        self.acc = []
+        self.res = ''
+        
+
+        print('zadanie 1: 10 najczęsciej występowanych słów oraz ich liczba')
+        self.most_common()
+        print(self.acc[:10])
+        
+        print('zadanie 2: zwróc 3 najczęstsze rzeczowniki')
+        self.nouns()
+        print(self.res[:3])
+        
+    def most_common(self, acc=[]):
+        for poem in range(1, 10):
+            req = requests.get("https://literat.ug.edu.pl/roxolan/00{}.htm".format(poem)) if poem < 10 else requests.get("https://literat.ug.edu.pl/roxolan/0{}.htm".format(poem))
+            text = BeautifulSoup(req.content, "lxml").text.split()
+            self.acc += text[2:len(text)-2]
+            
+        self.acc = Counter(self.acc).most_common(100)
+
     
-    return Counter(acc).most_common(10)
-    
-print(most_common()) # [('w', 248), ('z', 235), ('nie', 213), ('się', 171), ('na', 167), ('i', 141), ('Nie', 111), ('I', 91), ('A', 86), ('do', 79)]
+    def nouns(self):
+        for word in range(100):
+            translated_text = self.translator.translate(self.acc[word][0], dest="en")
+            if len(translated_text.text) > 2:
+                self.res += translated_text.text + ' '
+
+        nlp = spacy.load("en_core_web_sm")
+        doc = nlp(self.res)
+
+        #print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+        #print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+        
+        self.res = [token.lemma_ for token in doc if token.pos_ == "NOUN"]
+
+
+if __name__ == '__main__':
+    Common_Nouns()
 
 # Autor Wiktor Maj
